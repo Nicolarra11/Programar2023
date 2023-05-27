ae6bb70781d46d9eb70b03c1de9aec3d0da539cc
Nicolás Basualdo Larrazábal <nico.basualdo11@hotmail.com>
2023-05-27 15:08:53 -0300

    Practico n0 terminado

---
+++ promedio3.js
@@ -0,0 +1,37 @@
+/*
+4. Escribir un programa JavaScript que calcule el promedio de 3 números. Deberá
+guardar este programa en un archivo llamado: promedio3.js. Luego, responder
+a las siguientes preguntas utilizando su código para obtener los resultados.
+*/
+
+//a. ¿Cuál es el promedio entre 5, 7 y 9?
+
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+
+let promedio3 = (num1 + num2 + num3) / 3;
+
+console.log(promedio3);
+
+
+//b. ¿Cuál es el promedio entre -2 , -3, -8?
+
+ num1 = -2;
+ num2 = -3;
+ num3 = -8;
+
+ promedio3 = (num1 + num2 + num3) / 3;
+
+console.log(promedio3);
+
+
+//c. ¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+
+ num1 = -1.2;
+ num2 = 5.3;
+ num3 = -3.3;
+
+ promedio3 = (num1 + num2 + num3) / 3;
+
+console.log(promedio3);
