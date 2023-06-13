2ccd78a679b90a99beba00c515a57ef728c47f05
Nicolás Basualdo Larrazábal <nico.basualdo11@hotmail.com>
2023-06-12 23:40:11 -0300

    Carpeta Practico1 y PARTE 1: ejercicio1

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,24 @@
+/*
+1. Escribir un programa que:
+a. Defina una variable nombre.
+b. Guarde en la variable nombre tu nombre real.
+c. Defina y asigne, en un mismo paso, la variable apellido. El valor a
+asignar deberá ser tu apellido real.
+d. Repita el paso anterior, pero esta vez definiendo la variable edad y
+asignandole tu edad real.
+e. Por último, imprime por consola un mensaje que respete el siguiente
+formato:
+"Hola, [nombre] [apellido]. Tienes [edad] años."
+Este programa deberá ser guardado en un archivo llamado
+miNombreCompleto.js
+*/
+//a.
+let nombre;
+//b.
+nombre = "Nicolás";
+//c.
+let apellido = "Basualdo Larrazábal";
+//d.
+let edad = 28;
+//e.
+console.log ("Hola, "+[nombre]+" "+[apellido]+". Tienes "+[edad]+" años.");
